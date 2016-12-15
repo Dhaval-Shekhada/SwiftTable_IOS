2f5a98dd0db7aec780945e6e4735c9fb739da36a
Sarika D Shekhada <sarika_shekhada@sarikas-macmini>
2016-12-15 10:58:06 +0100

    Initial Commit

---
+++ tabswift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabswift
+//
+//  Created by Sarika D Shekhada on 15/12/16.
+//  Copyright © 2016 sarika. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
